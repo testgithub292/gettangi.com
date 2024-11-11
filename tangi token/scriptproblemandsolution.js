ff6fc92cfb490ba8ce77e7ee2fcd74c5c452a24a
testgithub292 <139734979+testgithub292@users.noreply.github.com>
2024-11-11 23:55:09 +0500

    update page

--- tangi token/scriptproblemandsolution.js
+++
@@ -1,8 +0,0 @@
-  /*---second section 3-------------------------------------------------------------*/
-
-   // Create an intersection observer
-   const observer = new IntersectionObserver(handleIntersection);
-
-   // Observe the heading and content
-   observer.observe(document.getElementById('heading_section3'));
-   observer.observe(document.getElementById('content_section3'));
