d676a0023d7a924c242e4561dc2c79b9abd9eb35
testgithub292 <139734979+testgithub292@users.noreply.github.com>
2024-11-13 22:58:07 +0500

    13/11-24
    today last update

---
+++ project_info/project info.js
@@ -0,0 +1,7 @@
+  
+  
+  /*-------road map script ---------------------------*/
+  // Show the paragraph with a transition effect
+  window.onload = function() {
+    document.getElementById('infoParagraph').classList.add('show');
+};
